9c2130d1afe9c98cf248af2138437faa911c96da
cely <146735843+cel3788@users.noreply.github.com>
2024-02-11 21:55:24 +0000

    changes file

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-"""
-    Insert here module comment
-
-    Write a function that prints My name is <first name> <last name>
-
-    Prototype: def say_my_name(first_name, last_name=""):
-    first_name and last_name must be strings otherwise,
-    raise a TypeError exception with the message
-    first_name must be a string or last_name must be a string
-    You are not allowed to import any module
-
-"""
+"""Module for say_my_name method."""
 
 
 def say_my_name(first_name, last_name=""):
-    """ print my first and last name """
-    str_error = "first_name must be a string or last_name must be a string"
-    if type(first_name) is not str or type(last_name) is not str:
-        raise TypeError(str_error)
-    print("My name is", first_name, last_name)
+    """Method for printing first and last name.
+
+    Args:
+        first_name: first name string.
+        last_name: last name string.
+
+    Raises:
+        TypeError: If first_name or last_name are not strings.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
