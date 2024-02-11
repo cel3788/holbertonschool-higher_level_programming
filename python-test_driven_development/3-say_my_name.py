865cd258e2eb80a5512fa8112b1f8dd288b23252
cely <146735843+cel3788@users.noreply.github.com>
2024-02-11 21:29:07 +0000

    init commit

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 """
-This module contains the function say_my_name.
+    Insert here module comment
+
+    Write a function that prints My name is <first name> <last name>
+
+    Prototype: def say_my_name(first_name, last_name=""):
+    first_name and last_name must be strings otherwise,
+    raise a TypeError exception with the message
+    first_name must be a string or last_name must be a string
+    You are not allowed to import any module
+
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints My name is <first name> <last name>
-        Args:
-            first_name(str): first name
-            lest_name(str): last name
-        Raises:
-            TypeError: first_name must be a string
-            TypeError: last_name must be a string
-    """
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-
-    if last_name:
-        print("My name is {} {}".format(first_name, last_name))
-    else:
-        print("My name is {}".format(first_name))
+    """ print my first and last name """
+    str_error = "first_name must be a string or last_name must be a string"
+    if type(first_name) is not str or type(last_name) is not str:
+        raise TypeError(str_error)
+    print("My name is", first_name, last_name)
