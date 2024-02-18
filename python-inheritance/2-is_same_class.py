49efdaa1dd009e7992ce573195df2e037c23972a
cely <146735843+cel3788@users.noreply.github.com>
2024-02-19 00:17:50 +0100

    Update 2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-'''Module for is_same_class method.'''
+"""
+Module that contains is_same_class
+checks if object is instance of specified class.
+"""
 
 
 def is_same_class(obj, a_class):
-    '''Determines if an object is exactly an instance of a class.'''
-    return type(obj) == a_class
+    """returns True if object is instance of specified class
+    False otherwise."""
+    return type(obj) is a_class
