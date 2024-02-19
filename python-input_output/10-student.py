d27a0bb6b09300ad80f8901baaa10ba81e59c418
cely <146735843+cel3788@users.noreply.github.com>
2024-02-19 02:12:26 +0100

    Update 10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,37 +1,4 @@
-#!/usr/bin/python3
-"""
-This module defines a Student class.
-"""
+import main
 
-
-class Student:
-    """A class that defines a student by first name, last name, and age."""
-
-    def __init__(self, first_name, last_name, age):
-        """Initialize a new Student with first_name, last_name, and age.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self, attrs=None):
-        """Retrieve a dictionary representation of a Student instance.
-
-        If attrs is a list of strings, only attribute names contained in this
-        list are retrieved. Otherwise, all attributes are retrieved.
-
-        Args:
-            attrs (list): A list of strings representing the names of the
-            attributes to retrieve.
-
-        Returns:
-            dict: A dictionary representation of the Student instance.
-        """
-        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
-        return self.__dict__
+student1 = main.Student("Alice", "Smith", 21)
+print(student1.to_json())
