1535b3c78b0886883bbfe6560d47179601c51686
cely <146735843+cel3788@users.noreply.github.com>
2024-02-19 02:03:22 +0100

    Update 10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,10 +1,34 @@
 #!/usr/bin/python3
-'''Module for class_to_json method.'''
+"""Defines a class Student."""
 
 
-def class_to_json(obj):
-    '''Returns dictionary description of json object.'''
-    if hasattr(obj, "__dict__"):
-        return obj.__dict__.copy()
-    else:
-        return {}
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+
+        If attrs is a list of strings, only attribute names contained in this list must be retrieved.
+        Otherwise, all attributes must be retrieved.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+
+        Returns:
+            dict: The dictionary representation of the Student.
+        """
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
