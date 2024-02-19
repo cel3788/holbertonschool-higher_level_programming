8f68b29042e92471c7c116051ebbcf66cc91741c
cely <146735843+cel3788@users.noreply.github.com>
2024-02-19 02:00:03 +0000

    exec file

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,21 +1,4 @@
-#!/usr/bin/python3
-"""
-Method module
-"""
-
 def inherits_from(obj, a_class):
-    """
-    Check if object is an instance of a class.
-    
-    Args:
-        obj: Object to check.
-        a_class: Class to check.
-    
-    Returns:
-        True or False.
-    """
-
     if type(obj) == a_class:
         return False
     return issubclass(type(obj), a_class)
-
