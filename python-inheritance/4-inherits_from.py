267ba7aa3f05df969f8a70b433e7f8f75c726e25
cely <146735843+cel3788@users.noreply.github.com>
2024-02-19 03:37:17 +0100

    Update 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,4 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that contains inherits_from
+taht checks if object is instance of a class
+or of class that inherited from this class
+"""
+
+
 def inherits_from(obj, a_class):
-    if type(obj) == a_class:
+    """returns True if object is instance of specified class
+    or instance of class that inherited from the class
+    False otherwise."""
+    obj_class = type(obj)
+    if obj_class == a_class:
         return False
-    return issubclass(type(obj), a_class)
+    return issubclass(obj_class, a_class)
