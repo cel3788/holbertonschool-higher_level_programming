b5e16017b1dda7a932f732331d2a02f7976781ec
cely <146735843+cel3788@users.noreply.github.com>
2024-02-19 02:19:10 +0100

    Update 10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -2,5 +2,13 @@
 
 import main
 
-student1 = main.Student("Alice", "Smith", 21)
-print(student1.to_json())
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
