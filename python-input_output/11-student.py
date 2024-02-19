c2863a22497359037cf55b51c3d7412117a0385b
cely <146735843+cel3788@users.noreply.github.com>
2024-02-19 01:59:42 +0100

    Update 11-student.py

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,11 +1,34 @@
-class Student:
-    '''Class for jsonification.'''
+#!/usr/bin/python3
+"""
+This module contains the class Student.
+"""
+
+
+class Student():
+    """
+    This class defines a student.
+    Attributes:
+        first_name: public instance attribute,
+        last_name: public instance attribute
+        age: public instance attribute
+    """
+
     def __init__(self, first_name, last_name, age):
-        '''Constructor.'''
-        self.first_name = first_name
+        """
+        Initialisation of the attributes of the class
+        initializes Student with first_name, last_name and age.
+        """
         self.last_name = last_name
+        self.first_name = first_name
         self.age = age
 
     def to_json(self):
-        '''Retrieves dictionary.'''
-        return self.__dict__.copy()
+        """Retrieves dictionary representation of Student instance.
+        """
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ replaces all attributes of the Student instance"""
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
