06ed506bcbcc9133e731864b083edbc177d579bb
cely <146735843+cel3788@users.noreply.github.com>
2024-02-19 01:15:24 +0100

    Update 10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,9 +1,9 @@
 def to_json(self, attrs=None):
     """Get a dictionary representation of the Student.
-
+    
     If attrs is a list of strings, represents only those attributes
     included in the list.
-
+    
     Args:
         attrs (list): (Optional) The attributes to represent.
     """
