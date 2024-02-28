6e9a8469e45047d3eaee5e195213fa5e722647d2
cely <146735843+cel3788@users.noreply.github.com>
2024-02-28 02:23:56 +0100

    Update 10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -7,6 +7,7 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initialize a new Student.
+
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
@@ -18,17 +19,14 @@ class Student:
 
     def to_json(self, attrs=None):
         """Get a dictionary representation of the Student.
+
         If attrs is a list of strings, represents only those attributes
         included in the list.
+
         Args:
             attrs (list): (Optional) The attributes to represent.
         """
-<<<<<<< HEAD
-           if (isinstance(attrs, list) and
-            all(isinstance(ele, str) for ele in attrs)):
-=======
-        if (isinstance(attrs, list) and
-                all(isinstance(ele, str) for ele in attrs)):
->>>>>>> refs/remotes/origin/main
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
